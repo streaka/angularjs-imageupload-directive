1671d8bbe1375fe77337dee31d118835e5d66551
Paul Iannazzo <somethingitalian@gmail.com>
2014-03-24 17:21:03 -0400

    restructured the repo into /src /demo folders

--- demo/back-end/app.js
+++ demo/back-end/app.js
@@ -6,20 +6,28 @@ var fs      = require('fs');
 
 var app = express();
 
+/*
+  it is expected that this app is run from the root dir via 
+  `node demo/back-end/app.js`
+*/
+
+var image_download_dir = 'demo/back-end/images';
+var front_end_dir = 'demo/front-end';
+
 app.configure(function() {
   app.set('port', process.env.PORT || 8080);
   app.use(express.bodyParser());
-  app.use(express.static(path.join(__dirname, 'public')));    
+  app.use(express.static(path.join(__dirname, image_download_dir)));    
 });
 
 
 app.get('/', function(req, res){
-  res.sendfile(__dirname + '/demo.html');
+  res.sendfile(__dirname + front_end_dir + 'demo.html');
 });
 
 app.post('/upload', function(req, res) {
   var image =  req.files.image;
-  var newImageLocation = path.join(__dirname, 'public/images', image.name);
+  var newImageLocation = path.join(__dirname, image_download_dir , image.name);
   
   fs.readFile(image.path, function(err, data) {
     fs.writeFile(newImageLocation, data, function(err) {
