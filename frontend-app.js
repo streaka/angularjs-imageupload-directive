80a547aecbb9257ebc62072a06a54a8216ca6127
Paul Iannazzo <somethingitalian@gmail.com>
2014-03-24 17:11:38 -0400

    moved the angularjs app out of the demo html

---
+++ frontend-app.js
@@ -0,0 +1,17 @@
+angular.module('imageuploadDemo', ['imageupload'])
+  .controller('DemoCtrl', function($scope, $http) {
+
+    $scope.single = function(image) {
+      var formData = new FormData();
+      formData.append('image', image, image.name);
+
+      $http.post('upload', formData, {
+        headers: { 'Content-Type': false },
+        transformRequest: angular.identity
+      }).success(function(result) {
+        $scope.uploadedImgSrc = result.src;
+        $scope.sizeInBytes = result.size;
+      });
+    };
+
+  });
