2b49515f1360e27340f1b3458b3dbd2ab0f4f62d
Paul Iannazzo <somethingitalian@gmail.com>
2014-03-24 17:02:05 -0400

    indenting

--- app.js
+++ app.js
@@ -7,30 +7,30 @@ var fs      = require('fs');
 var app = express();
 
 app.configure(function() {
-    app.set('port', process.env.PORT || 8080);
-    app.use(express.bodyParser());
-    app.use(express.static(path.join(__dirname, 'public')));    
+  app.set('port', process.env.PORT || 8080);
+  app.use(express.bodyParser());
+  app.use(express.static(path.join(__dirname, 'public')));    
 });
 
 
 app.get('/', function(req, res){
-    res.sendfile(__dirname + '/demo.html');
+  res.sendfile(__dirname + '/demo.html');
 });
 
 app.post('/upload', function(req, res) {
-    var image =  req.files.image;
-    var newImageLocation = path.join(__dirname, 'public/images', image.name);
-    
-    fs.readFile(image.path, function(err, data) {
-        fs.writeFile(newImageLocation, data, function(err) {
-            res.json(200, { 
-                src: 'images/' + image.name,
-                size: image.size
-            });
-        });
+  var image =  req.files.image;
+  var newImageLocation = path.join(__dirname, 'public/images', image.name);
+  
+  fs.readFile(image.path, function(err, data) {
+    fs.writeFile(newImageLocation, data, function(err) {
+      res.json(200, { 
+        src: 'images/' + image.name,
+        size: image.size
+      });
     });
+  });
 });
 
 http.createServer(app).listen(app.get('port'), function() {
-    console.log("imageupload demo running on port " + app.get('port'));
+  console.log("imageupload demo running on port " + app.get('port'));
 });
